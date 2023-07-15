8ebc5c0abdbd325c64109b917615af059cd215c3
Jaimayal <jaime.ayalaa.76@gmail.com>
2023-07-15 09:10:00 -0600

    Added custom colors to tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,18 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				"moderate-blue": "#5457b6",
+				"soft-red": "#ed6468",
+				"light-grayish-blue": "	#c3c4ef",
+				"pale-red": "#ffb8bb",
+				"dark-blue": "#324152",
+				"grayish-blue": "	#67727e",
+				"light-gray": "#eaecf1",
+				"ultra-light-gray": "#f5f6fa",
+			}
+		},
 	},
 	plugins: [],
 }
